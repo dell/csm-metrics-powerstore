fff17718acf12f4df327b90bdc72100476836442
Trevor Dawe <trevor.dawe@dell.com>
2021-04-30 11:54:51 -0400

    Added comments for gosec exclusion

--- scripts/check.sh
+++ scripts/check.sh
@@ -44,6 +44,7 @@ LINT_RETURN_CODE=$?
 echo === Finished
 
 echo === Running gosec...
+# exclude "G108: Profiling endpoint is automatically exposed" because we are exposing on an internal port
 gosec --exclude=G108 -quiet ${MOD_FLAGS} ${CHECK_DIRS}
 SEC_RETURN_CODE=$?
 echo === Finished
