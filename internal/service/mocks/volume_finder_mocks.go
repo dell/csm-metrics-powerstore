33fa8885f33d5b930fedeced93b6712c1e412872
Tao He <tao.he1@dell.com>
2022-07-01 15:40:34 +0800

    upgrade OTEL version to v1.7.0.

--- internal/service/mocks/volume_finder_mocks.go
+++ internal/service/mocks/volume_finder_mocks.go
@@ -6,35 +6,36 @@ package mocks
 
 import (
 	context "context"
+	reflect "reflect"
+
 	k8s "github.com/dell/csm-metrics-powerstore/internal/k8s"
 	gomock "github.com/golang/mock/gomock"
-	reflect "reflect"
 )
 
-// MockVolumeFinder is a mock of VolumeFinder interface
+// MockVolumeFinder is a mock of VolumeFinder interface.
 type MockVolumeFinder struct {
 	ctrl     *gomock.Controller
 	recorder *MockVolumeFinderMockRecorder
 }
 
-// MockVolumeFinderMockRecorder is the mock recorder for MockVolumeFinder
+// MockVolumeFinderMockRecorder is the mock recorder for MockVolumeFinder.
 type MockVolumeFinderMockRecorder struct {
 	mock *MockVolumeFinder
 }
 
-// NewMockVolumeFinder creates a new mock instance
+// NewMockVolumeFinder creates a new mock instance.
 func NewMockVolumeFinder(ctrl *gomock.Controller) *MockVolumeFinder {
 	mock := &MockVolumeFinder{ctrl: ctrl}
 	mock.recorder = &MockVolumeFinderMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockVolumeFinder) EXPECT() *MockVolumeFinderMockRecorder {
 	return m.recorder
 }
 
-// GetPersistentVolumes mocks base method
+// GetPersistentVolumes mocks base method.
 func (m *MockVolumeFinder) GetPersistentVolumes(arg0 context.Context) ([]k8s.VolumeInfo, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetPersistentVolumes", arg0)
@@ -43,7 +44,7 @@ func (m *MockVolumeFinder) GetPersistentVolumes(arg0 context.Context) ([]k8s.Vol
 	return ret0, ret1
 }
 
-// GetPersistentVolumes indicates an expected call of GetPersistentVolumes
+// GetPersistentVolumes indicates an expected call of GetPersistentVolumes.
 func (mr *MockVolumeFinderMockRecorder) GetPersistentVolumes(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetPersistentVolumes", reflect.TypeOf((*MockVolumeFinder)(nil).GetPersistentVolumes), arg0)
